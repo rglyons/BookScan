1b3e9b5d7c2c2e516eb75bcdf84a5ae2b80a2b5a
RobertLyons <rglyons@ucsc.edu>
2015-10-24 13:59:40 -0700

    Scan populates listView with img and ISBN

--- app/src/main/java/com/example/robertlyons/bookscan/MainActivity.java
+++ app/src/main/java/com/example/robertlyons/bookscan/MainActivity.java
@@ -1,17 +1,115 @@
 package com.example.robertlyons.bookscan;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.robertlyons.bookscan.ImageLoadTask;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ArrayList<custom_list> aList;
+
+    String Philips_url = "http://ecx.images-amazon.com/images/I/51m17Nl8C-L._SL160_.jpg";
+
+    private class custom_list {
+
+        custom_list() {};
+
+        String ivURL;
+        String tvContent;
+    }
+
+    private class MyAdapter extends ArrayAdapter<custom_list> {
+
+        int resource;
+        Context context;
+
+
+        public MyAdapter(Context _context, int _resource, List<custom_list> items) {
+            super(_context, _resource, items);
+            resource = _resource;
+            context = _context;
+            this.context = _context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout newView;
+
+            custom_list w = getItem(position);
+
+            // Inflate a new view if necessary.
+            if (convertView == null) {
+                newView = new LinearLayout(getContext());
+                String inflater = Context.LAYOUT_INFLATER_SERVICE;
+                LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+                vi.inflate(resource,  newView, true);
+            } else {
+                newView = (LinearLayout) convertView;
+            }
+
+            // Fills in the view.
+            TextView bookDesc = (TextView) newView.findViewById(R.id.book_desc);
+            bookDesc.setText(w.tvContent);
+
+            ImageView bookPic = (ImageView) newView.findViewById(R.id.book_picture);
+            new ImageLoadTask(Philips_url, bookPic).execute();
+
+            //Set a listener for the whole list item.
+            /*newView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String id =  v.getTag().toString();
+                    //Log.i(LOG_TAG, "id is: " + id);
+                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+                    SharedPreferences.Editor editor = settings.edit();
+                }
+            });*/
+            return newView;
+        }
+    }
+
+    private MyAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aList = new ArrayList<custom_list>();
+        aa = new MyAdapter(this, R.layout.custom_list, aList);
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(aa);
+        aa.notifyDataSetChanged();
+
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        startActivityForResult(intent, 0);
     }
 
 
@@ -36,4 +134,43 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        if(requestCode == 0){
+            if(resultCode == RESULT_OK){
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Log.i("xZing", "contents: " + contents + " format: " + format);
+                // Handle successful scan
+                custom_list cl = new custom_list();
+                cl.tvContent = contents;
+                //cl.ivContent = ;
+                aList.add(cl);
+                aa.notifyDataSetChanged();
+            }
+            else if(resultCode == RESULT_CANCELED){
+                // Handle cancel
+                Log.i("xZing", "Cancelled");
+            }
+        }
+    }
+/*
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src",src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
+    }
+*/
 }
